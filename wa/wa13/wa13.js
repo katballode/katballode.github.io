e95b3312560142b5a4359395f829611ad8523039
katballode <123122101+katballode@users.noreply.github.com>
2023-04-23 20:29:17 -0600

    wa13 done

--- wa/wa13/wa13.js
+++ wa/wa13/wa13.js
@@ -1,26 +1,15 @@
-function update() {
-    let currentValue = slider.value;
-    if (currentValue < 1) {
-        currentValue = 1;
-        slider.value = currentValue;
-    } else if (currentValue > 99) {
-        currentValue = 99;
-        slider.value = currentValue;
-    }
-    if (currentValue % 2 === 0) {
-        currentValue = parseInt(currentValue) + 1;
-        slider.value = currentValue;
-    }
-    sliderOutput.textContent = currentValue;
-    if (currentValue % 2 !== 0) {
-        submit();
-    }
-}
-
-function submit() {
-    alert("Your volume is now: " + sliderOutput.textContent);
-}
-
 const slider = document.getElementById("myRange");
-const sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
-const sliderOutput = document.querySelector(".slider-output");
+const output = document.querySelector(".slider-output");
+const submitBtn = document.getElementById("submitBtn");
+
+output.innerText = `Volume: ${slider.value}`;
+
+slider.addEventListener("input", () => {
+    output.innerText = `Volume: ${slider.value}`;
+});
+
+submitBtn.addEventListener("click", () => {
+    alert(`No, I like the number 50, so it will be 50. Sorry not sorry.`);
+    slider.value = 50;
+    output.innerText = `Volume: ${slider.value}`;
+});
